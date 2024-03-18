22d20f70d4594e064362f7c018fe854d5c7b9064
unknown <esteban.sierrapatino@students.fhnw.ch>
2024-03-18 11:32:56 -0500

    creacion de robots por terminal

--- Trabajos/MiPrimerRobot.java
+++ Trabajos/MiPrimerRobot.java
@@ -1,28 +1,76 @@
 import kareltherobot.*;
 import java.awt.Color;
+import org.apache.commons.cli.*;
 
 public class MiPrimerRobot implements Directions {
     public static void main(String[] args) {
-        // Usamos el archivo que creamos del mundo
         World.readWorld("Mundo-Final.kwld");
         World.setVisible(true);
-
         Color blue = new Color(0, 255, 225);
         Color red = new Color(255, 0, 0);
         Color black = new Color(0, 0, 0);
-        Racer Minero1 = new Racer(8, 1, South, 0, black);
-        Racer Minero2 = new Racer(9, 1, South, 0, black);
-        Racer Tren1 = new Racer(10, 1, South, 0, blue);
-        Racer Tren2 = new Racer(11, 1, South, 0, blue);
-        Racer Tren3 = new Racer(12, 1, South, 0, blue);
-        Racer Extractor = new Racer(13, 1, South, 0, red);
 
-        Minero1.run();
-        Minero2.run();
-        Tren1.run();
-        Tren2.run();
-        Tren3.run();
-        Extractor.run();
+        // Parseamos los argumentos de la línea de comandos
+        CommandLine cmd = parseCommandLine(args);
+
+        // Obtenemos la cantidad de cada tipo de robot
+        int cantidadMineros = Integer.parseInt(cmd.getOptionValue("m", "2"));
+        int cantidadTransportadores = Integer.parseInt(cmd.getOptionValue("t", "2"));
+        int cantidadExploradores = Integer.parseInt(cmd.getOptionValue("e", "2"));
+
+        // Creamos los robots según la cantidad especificada
+        crearRobots(cantidadMineros, cantidadTransportadores, cantidadExploradores, blue, red, black);
+    }
+
+    private static CommandLine parseCommandLine(String[] args) {
+        Options options = new Options();
+
+        // Definir las opciones de línea de comandos
+        Option minero = Option.builder("m")
+                              .argName("cantidad_minerales")
+                              .hasArg()
+                              .desc("Cantidad de mineros (por defecto: 2)")
+                              .build();
+
+        Option transportador = Option.builder("t")
+                                     .argName("cantidad_transportadores")
+                                     .hasArg()
+                                     .desc("Cantidad de transportadores (por defecto: 2)")
+                                     .build();
+
+        Option explorador = Option.builder("e")
+                                   .argName("cantidad_exploradores")
+                                   .hasArg()
+                                   .desc("Cantidad de exploradores (por defecto: 2)")
+                                   .build();
+
+        options.addOption(minero);
+        options.addOption(transportador);
+        options.addOption(explorador);
+
+        CommandLineParser parser = new DefaultParser();
+        try {
+            return parser.parse(options, args);
+        } catch (ParseException e) {
+            System.err.println("Error al analizar los argumentos de la línea de comandos: " + e.getMessage());
+            System.exit(1);
+            return null;
+        }
+    }
+
+    private static void crearRobots(int cantidadMineros, int cantidadTransportadores, int cantidadExploradores,
+                                    Color blue, Color red, Color black) {
+        for (int i = 0; i < cantidadMineros; i++) {
+            new Racer(8 + i, 1, South, 0, black).run();
+        }
+
+        for (int i = 0; i < cantidadTransportadores; i++) {
+            new Racer(10 + i, 1, South, 0, blue).run();
+        }
+
+        for (int i = 0; i < cantidadExploradores; i++) {
+            new Racer(13 + i, 1, South, 0, red).run();
+        }
     }
 }
 
