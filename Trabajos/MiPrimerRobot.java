b7a8983e77f3af33ec1b8ae16599bf0f6cdba50f
unknown <esteban.sierrapatino@students.fhnw.ch>
2024-03-18 15:45:49 -0500

    extractores funcionando y codigo para generar los otros robots (no implementado la configuracion)

--- Trabajos/MiPrimerRobot.java
+++ Trabajos/MiPrimerRobot.java
@@ -11,8 +11,8 @@ public class MiPrimerRobot implements Directions {
 
         // Definimos la cantidad predeterminada de robots si no se especifica por línea de comandos
         int cantidadMineros = 2;
-        int cantidadTransportadores = 2;
-        int cantidadExploradores = 2;
+        int cantidadTrenes = 2;
+        int cantidadExtractores = 2;
 
         // Verificamos si se especificaron argumentos de línea de comandos
         if (args.length >= 6 && args.length % 2 == 0) {
@@ -24,10 +24,10 @@ public class MiPrimerRobot implements Directions {
                         cantidadMineros = Integer.parseInt(value);
                         break;
                     case "-t":
-                        cantidadTransportadores = Integer.parseInt(value);
+                        cantidadTrenes = Integer.parseInt(value);
                         break;
                     case "-e":
-                        cantidadExploradores = Integer.parseInt(value);
+                        cantidadExtractores = Integer.parseInt(value);
                         break;
                     default:
                         System.out.println("Opción no reconocida: " + option);
@@ -38,7 +38,10 @@ public class MiPrimerRobot implements Directions {
         }
 
         // Creamos los robots según la cantidad especificada
-        crearRobots(cantidadMineros, cantidadTransportadores, cantidadExploradores, blue, red, black);
+        crearRobots(cantidadMineros, cantidadTrenes, cantidadExtractores, blue, red, black);
+        mineros(cantidadMineros);
+        trenes(cantidadTrenes);
+        extractores(cantidadExtractores);
     }
 
     private static void crearRobots(int cantidadMineros, int cantidadTransportadores, int cantidadExploradores,
@@ -54,6 +57,75 @@ public class MiPrimerRobot implements Directions {
         for (int i = 0; i < cantidadExploradores; i++) {
             new Racer(13 + i, 1, South, 0, red).run();
         }
+    }
+
+    private static void extractores(int cantidadExtractores) {
+        int puntoExtraccion = 11;
+        int avenidaSuperficie = 2;
+
+        // Crear y ejecutar el comportamiento de los extractores
+        for (int i = 0; i < cantidadExtractores; i++) {
+            Extractor extractor = new Extractor(puntoExtraccion, avenidaSuperficie, Color.RED);
+            extractor.extraerBeepers();
+            extractor.salirMina();
+        }
+    }
+
+    private static void mineros(int cantidadMineros) {
+
+        // Crear y ejecutar el comportamiento de los mineros
+        for (int i = 0; i < cantidadMineros; i++) {
+
+        }
+    }
+
+    private static void trenes(int cantidadTrenes) {
+
+        // Crear y ejecutar el comportamiento de los mineros
+        for (int i = 0; i < cantidadTrenes; i++) {
+
+        }
+    }
+}
+
+class Extractor extends Robot {
+    private static final int capacidad_max = 50;
+
+    public Extractor(int street, int avenue, Color color) {
+        super(street, avenue, North, 0, color);
+        World.setupThread(this);
+    }
+
+    public void extraerBeepers() {
+        int cantidadExtraida = 0;
+        while (nextToABeeper() && cantidadExtraida < capacidad_max) {
+            pickBeeper();
+            cantidadExtraida++;
+        }
+        System.out.println("El extractor ha extraído " + cantidadExtraida + " beepers.");
+    }
+
+    public void salirMina() {
+        while (!frontIsClear()) {
+            turnLeft();
+        }
+        move();
+        while (frontIsClear()) {
+            move();
+        }
+        turnLeft();
+        turnLeft();
+        while (frontIsClear()) {
+            move();
+        }
+        System.out.println("El extractor ha salido de la mina y se va a descansar.");
+        informarSalidaBodega();
+        turnOff();
+    }
+
+    private void informarSalidaBodega() {
+        // Informar a otros robots que pueden salir de la bodega
+        System.out.println("¡Los extractores pueden salir de la bodega!");
     }
 }
 
